65863e50f4059c8c3a24108d2b2dcebf8df05e92
sulan <sulan@LAPTOP-T5JAMT0I.dhcp.wustl.edu>
2022-10-20 12:39:19 -0500

    adsf

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -15,8 +15,11 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
-		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		double xDistance = x2- x1;
+		double yDistance = y2- y1;
+		xDistance = Math.pow(xDistance, 2);
+		yDistance = Math.pow(yDistance, 2);
+		distance = Math.sqrt(xDistance+yDistance);
 		return distance;
 	}
 
@@ -30,20 +33,23 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, radius*0.75);
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
-		// suggested rgb values: 0, 109, 219
+		// suggested rgb values: 
 
 		
-
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius*0.5);
 		// Red ring with 1.0/2.0 the radius
-		// suggested rgb values: 146, 0, 0
+		// suggested rgb values: 
 
 		
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius*0.25);
 		// Yellow ring with 1.0/4.0 the radius
-		// suggested rgb values: 255, 255, 109
+		// suggested rgb values: 
 
 		
 	}
@@ -61,8 +67,16 @@ public class Methods {
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
-		// TODO: Finish this method
-		
+		char[] sourceChar = source.toCharArray();
+		for (int i = 0; i < source.length(); i++)
+		{
+			if (sourceChar[i] == target)
+			{
+				source = source.substring(0,i) + replacement + source.substring(i+1,source.length());
+				sourceChar = source.toCharArray();
+			}
+			result = source;
+		}
 		return result;
 	}
 
@@ -74,11 +88,13 @@ public class Methods {
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
-		// FIXME: Compute the sum of the values in an array
-		
+		for (int a = 0; a < (values.length); a++)
+		{
+			sum = sum + values[a];
+		}
 		return sum;
 	}
-
+		
 	/**
 	 * Return an array of a given size filled with the provided value
 	 * 
@@ -87,16 +103,36 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
+		for (int b = 0; b<length; b++)
+		{
+			values[b] = value;
+		}
 		// TODO: Finish this method
-
 		
 
 		return values;
 	}
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
-	// TODO: Create a JavaDoc comment for the arrayMean method.
-
+	/**
+	 * Return an array of a given size filled with the provided value
+	 * 
+	 * @param length the length of the given array
+	 * @param value  the value to fill the array with
+	 * @return and array of size that's filled with value
+	 */
+	public static double arrayMean(int[] values) 
+	{
+		double N = values.length;
+		double sum = 0;
+		for (int a = 0; a < (values.length); a++)
+		{
+			sum = sum + values[a];
+		}
+		
+		double mean = sum / N;
+		return mean;
+	}
 	
 }
